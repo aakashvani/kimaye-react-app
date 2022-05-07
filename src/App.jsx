f6dea5e9624f0437122be7dc4496feef17762780
Omkar Londhe <imomkarlondhe@gmail.com>
2022-05-07 12:21:36 +0530

    cartPage added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ProductsDetailPage from "./pages/ProductsDetailPage/ProductsDetailPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import SignPage from "./pages/Signpage/SignPage";
 import PaymentPage from "./pages/Paymentpage/PaymentPage";
+import CartPage from "./pages/CartPage/CartPage";
 import Footer from "./components/Footer/footer";
 
 import { Route, Routes } from "react-router-dom";
@@ -16,7 +17,10 @@ const App = () => (
       <Routes>
         <Route path="/" element={<LandingPage />} exact></Route>
         <Route path="/ProductsPage" element={<ProductsPage />}></Route>
-        <Route path="/ProductsDetailPage" element={<ProductsDetailPage />}></Route>
+        <Route
+          path="/ProductsDetailPage"
+          element={<ProductsDetailPage />}
+        ></Route>
         <Route path="/SignPage" element={<SignPage />}></Route>
         <Route path="/LoginPage" element={<LoginPage />}></Route>
         <Route path="/CartPage" element={<CartPage />}></Route>
