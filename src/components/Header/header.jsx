0893729663f57f7273d2e4b9946847484f1822cf
ROHIT CHOUREY <97465195+rohitchourey0809@users.noreply.github.com>
2022-05-08 15:40:56 +0530

    ok

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -9,8 +9,8 @@ import {
 
 import { Link } from "react-router-dom";
 // import { Button } from "@chakra-ui/react";
-import { ColorModeSwitcher } from "../../ColorModeSwitcher";
-import { Button } from "@chakra-ui/react";
+// import { ColorModeSwitcher } from "../../ColorModeSwitcher";
+// import { Button } from "@chakra-ui/react";
 
 const header = () => {
   return (
@@ -19,11 +19,11 @@ const header = () => {
         {/* left menu-bar  */}
         <div className="left-menu">
           <ul className="menu">
-            <li>
+            {/* <li>
               <Button>
                 <ColorModeSwitcher />
               </Button>
-            </li>
+            </li> */}
             <Link
               style={{ textDecoration: "none", color: "black" }}
               to="/allfruits"
